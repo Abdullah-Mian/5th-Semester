51be332b89014a3a357342434257ee020ed41ed0
abdulah <aabdullahmian@gmail.com>
2024-11-22 02:05:25 +0500

    DB LAB 9

---
+++ DataBases/LAB/LAB9.sql
@@ -0,0 +1,111 @@
+USE Superhero;
+CREATE LOGIN JeanPaulJohnLogin WITH PASSWORD = '12341234';
+
+CREATE USER JeanPaulJohn FOR LOGIN JeanPaulJohnLogin;
+
+CREATE ROLE MarvelSuperheroManager;
+
+-- Create a view to look at all the features/characteristics of Marvel superheroes
+CREATE VIEW vw_MarvelSuperheroes AS
+SELECT 
+    s.id AS superhero_id,    
+    s.full_name,
+    s.superhero_name,
+    p.id AS publisher_id,    
+    p.publisher_name
+FROM 
+    superhero s
+JOIN 
+    publisher p ON s.publisher_id = p.id
+WHERE 
+    p.publisher_name LIKE '%Marvel%';
+GO
+-- Grant SELECT permission on the Marvel superheroes view to the role
+GRANT SELECT ON vw_MarvelSuperheroes TO MarvelSuperheroManager;
+
+SELECT * FROM vw_MarvelSuperheroes;
+
+
+-- Grant permission to add superheroes in the superhero table
+GRANT INSERT ON superhero TO MarvelSuperheroManager;
+
+-- Insert a new Marvel superhero
+INSERT INTO superhero (id, full_name, superhero_name, publisher_id)
+VALUES (101, 'Peter Parker', 'Spider-Man', (SELECT id FROM publisher WHERE publisher_name LIKE '%Marvel%'));
+
+
+
+-- Create a view to manage deletion of DC superheroes
+CREATE VIEW vw_DCSuperheroes AS
+SELECT 
+    s.id AS superhero_id,   
+    s.full_name,
+    s.superhero_name,
+    p.id AS publisher_id,   
+    p.publisher_name
+FROM 
+    superhero s
+JOIN 
+    publisher p ON s.publisher_id = p.id
+WHERE 
+    p.publisher_name LIKE '%DC%';
+GO
+-- Grant DELETE permission on the DC superheroes view to the role
+GRANT DELETE ON vw_DCSuperheroes TO MarvelSuperheroManager;
+-- Delete a DC superhero
+DELETE FROM vw_DCSuperheroes
+WHERE superhero_id = (
+    SELECT TOP 1 s.id
+    FROM superhero s
+    JOIN publisher p ON s.publisher_id = p.id
+    WHERE p.publisher_name LIKE '%DC%'
+);
+
+-- Grant the role the ability to delegate read privileges
+GRANT SELECT ON vw_MarvelSuperheroes TO MarvelSuperheroManager WITH GRANT OPTION;
+
+-- Add the user JeanPaulJohn to the MarvelSuperheroManager role
+ALTER ROLE MarvelSuperheroManager ADD MEMBER JeanPaulJohn;
+
+-----------------------------------------------------------------------------
+CREATE LOGIN RajnikanthPyarelalLogin WITH PASSWORD = '12341234';
+GO
+CREATE USER RajnikanthPyarelal FOR LOGIN RajnikanthPyarelalLogin;
+
+CREATE ROLE DCComicsManager;
+
+-- Create a view to look at all the features/characteristics of Marvel superheroes
+GRANT SELECT ON vw_MarvelSuperheroes TO DCComicsManager;
+
+-- Grant INSERT permission on the superhero table to the role
+GRANT INSERT ON superhero TO DCComicsManager;
+
+-- Use the existing view for DC superheroes (created earlier)
+GRANT DELETE ON vw_DCSuperheroes TO DCComicsManager;
+
+-- Add RajnikanthPyarelal to the DCComicsManager role
+ALTER ROLE DCComicsManager ADD MEMBER RajnikanthPyarelal;
+
+-------------------------------------------------------------------------------------
+CREATE LOGIN AlphonsoLogin WITH PASSWORD = '12341234';
+GO
+
+CREATE USER Alphonso FOR LOGIN AlphonsoLogin;
+
+-- Create a role for managing superhero attributes
+CREATE ROLE SuperheroAttributeManager;
+
+-- Grant see all the superheros 
+GRANT SELECT ON superhero TO SuperheroAttributeManager;
+
+-- Grant UPDATE permission for specific columns in the superhero table
+GRANT UPDATE (hair_colour_id, skin_colour_id, eye_colour_id) ON superhero TO SuperheroAttributeManager;
+GO
+-- Add Alphonso to the SuperheroAttributeManager role
+ALTER ROLE SuperheroAttributeManager ADD MEMBER Alphonso;
+
+
+
+
+
+
